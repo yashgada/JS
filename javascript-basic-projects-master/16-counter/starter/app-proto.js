e34015f19c7c2de10dd9c8b57a245cedc1933495
Yash Gada <gadayash9@gmail.com>
2022-06-02 01:13:47 +0530

    Done with counter

---
+++ javascript-basic-projects-master/16-counter/starter/app-proto.js
@@ -0,0 +1,51 @@
+// helper functions:
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    console.log(element);
+    return element;
+  }
+  throw new Error(
+    `Please check "${selection}" selector, no such element exists`
+  );
+}
+function Counter(element, value) {
+//   console.log(element, value);
+    this.counter = element
+    this.resetBtn = element.querySelector('.reset')
+    this.decreaseBtn = element.querySelector('.decrease')
+    this.increaseBtn = element.querySelector('.increase')
+    this.value = value
+    this.valueDOM = element.querySelector('.value')
+    this.valueDOM.textContent = this.value
+
+    // bind the functions correctly
+    // Here, this points to the instance of Counter
+    // But in addEventListener, any function called has its this point to the button. The context is by default the button
+    this.reset = this.reset.bind(this)
+    this.increase = this.increase.bind(this)
+    this.decrease = this.decrease.bind(this)
+
+    this.resetBtn.addEventListener('click', this.reset)
+    this.increaseBtn.addEventListener('click', this.increase)
+    this.decreaseBtn.addEventListener('click', this.decrease)
+}
+
+Counter.prototype.increase = function() {
+    console.log(this);
+    this.value++
+    this.valueDOM.textContent = this.value
+}
+Counter.prototype.decrease = function() {
+    console.log(this);
+    this.value--
+    this.valueDOM.textContent = this.value
+}
+Counter.prototype.reset = function() {
+    console.log(this);
+    this.value = 0
+    this.valueDOM.textContent = this.value
+}
+
+const firstCounter = new Counter(getElement(".first-counter"), 100);
+const secondCounter = new Counter(getElement(".second-counter"), 200);

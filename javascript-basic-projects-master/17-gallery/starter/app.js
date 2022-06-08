9346388b98d83e4e13922afe99fe2da57c5cc90e
Yash Gada <gadayash9@gmail.com>
2022-06-08 16:22:10 +0530

    Done with Gallery

--- javascript-basic-projects-master/17-gallery/starter/app.js
+++ javascript-basic-projects-master/17-gallery/starter/app.js
@@ -7,3 +7,91 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+function Gallery(element) {
+  this.container = element;
+  this.list = [...element.querySelectorAll(".img")];
+  // console.log(this.list);
+  this.modal = getElement(".modal");
+  this.modalImg = getElement(".main-img");
+  this.imageName = getElement('.image-name')
+  this.modalImages = getElement(".modal-images");
+  this.closeBtn = getElement(".close-btn");
+  this.prevBtn = getElement(".prev-btn");
+  this.nextBtn = getElement(".next-btn");
+  // self reference is an alternative to duck the "this" confusion and games. Switch comments on  and take a look.
+  let self = this;
+  // bind functions
+  // this.openModal = this.openModal.bind(this);
+  this.closeModal = this.closeModal.bind(this)
+  this.nextImage = this.nextImage.bind(this)
+  this.prevImage = this.prevImage.bind(this)
+  this.chooseImage = this.chooseImage.bind(this)
+
+  this.container.addEventListener(
+    "click",
+    function (e) {
+      // console.log(this);
+      if (e.target.classList.contains('img')) {
+        this.openModal(e.target,this.list);
+        // self.openModal();
+      }
+    }.bind(this)
+  );
+  // });
+}
+Gallery.prototype.openModal = function (selectedImage, list) {
+  // for the code here to work, the "this" needs to point to an instance of Gallery.
+  // console.log(this);
+  // console.log("from openModal");
+
+  this.setMainImage(selectedImage)
+
+  this.modalImages.innerHTML = list.map(function (image) {
+    return `<img src="${image.src}" title="${image.title}" data-id="${image.dataset.id}" class="${selectedImage.dataset.id===image.dataset.id?"modal-img selected":"modal-img"}"/>`
+  }).join("")
+  this.modal.classList.add("open");
+  this.closeBtn.addEventListener('click',this.closeModal)
+  this.nextBtn.addEventListener('click',this.nextImage)
+  this.prevBtn.addEventListener('click', this.prevImage)
+  this.modalImages.addEventListener('click', this.chooseImage)
+};
+
+Gallery.prototype.setMainImage = function (selectedImage) {
+  this.modalImg.src = selectedImage.src
+  this.imageName.textContent = selectedImage.title
+}
+
+Gallery.prototype.closeModal = function () {
+  this.modal.classList.remove('open')
+  this.closeBtn.removeEventListener("click", this.closeModal);
+  this.nextBtn.removeEventListener("click", this.nextImage);
+  this.prevBtn.removeEventListener("click", this.prevImage);
+}
+
+Gallery.prototype.nextImage = function () {
+  const selected = this.modalImages.querySelector('.selected')
+  const next = selected.nextElementSibling || this.modalImages.firstElementChild;
+  next.classList.add('selected')
+  selected.classList.remove('selected')
+  this.setMainImage(next)
+}
+Gallery.prototype.prevImage = function () {
+  const selected = this.modalImages.querySelector(".selected");
+  const prev =
+    selected.previousElementSibling || this.modalImages.lastElementChild;
+  prev.classList.add("selected");
+  selected.classList.remove("selected");
+  this.setMainImage(prev);
+}
+Gallery.prototype.chooseImage = function (e) {
+  if (e.target.classList.contains('modal-img')) {
+    const selected = this.modalImages.querySelector('.selected')
+    selected.classList.remove('selected')
+    e.target.classList.add('selected')
+    this.setMainImage(e.target)
+  }
+}
+
+const nature = new Gallery(getElement(".nature"));
+const city = new Gallery(getElement(".city"));

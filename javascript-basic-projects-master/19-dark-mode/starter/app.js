8c0b8c0751b0dcc0afb1757ea1ed344cad4e21f8
Yash Gada <gadayash9@gmail.com>
2022-06-12 02:57:23 +0530

    Done with dark mode

--- javascript-basic-projects-master/19-dark-mode/starter/app.js
+++ javascript-basic-projects-master/19-dark-mode/starter/app.js
@@ -1,1 +1,24 @@
-console.log('dark mode');
+const toggleBtn = document.querySelector(".btn");
+const articlesContainer = document.querySelector('.articles')
+
+articlesContainer.innerHTML = articles.map(({title, date, length, snippet}) => {
+    const formatDate = moment(date).format('MMMM Do, YYYY')
+    // console.log(formatDate);
+    return `
+    <!-- single post -->
+      <article class="post">
+        <h2>${title}</h2>
+        <div class="post-info">
+          <span class="date">${formatDate}</span>
+          <span class="read">${length} minute read</span>
+        </div>
+        <p>${snippet}</p>
+      </article>
+      <!-- end of single post -->
+    `;
+}).join('')
+
+toggleBtn.addEventListener("click", () => {
+    document.documentElement.classList.toggle('dark-theme')
+  console.log("Hi there");
+});

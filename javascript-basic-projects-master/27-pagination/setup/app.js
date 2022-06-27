9c0aefa8b4dfa6baaa46b32fdbf3cc60a5bd2068
Yash Gada <gadayash9@gmail.com>
2022-06-27 13:46:17 +0530

    Done with Pagination

--- javascript-basic-projects-master/27-pagination/setup/app.js
+++ javascript-basic-projects-master/27-pagination/setup/app.js
@@ -1,4 +1,53 @@
-import fetchFollowers from './fetchFollowers.js'
-import displayFollowers from './displayFollowers.js'
-import paginate from './paginate.js'
-import displayButtons from './displayButtons.js'
+import fetchFollowers from "./fetchFollowers.js";
+import displayFollowers from "./displayFollowers.js";
+import paginate from "./paginate.js";
+import displayButtons from "./displayButtons.js";
+
+const title = document.querySelector(".section-title h1");
+const btnContainer = document.querySelector(".btn-container");
+
+let index = 0;
+let pages = [];
+
+const setupUI = () => {
+  displayFollowers(pages[index]);
+  displayButtons(btnContainer, pages, index);
+    console.log('rendered');
+};
+
+const init = async () => {
+  const followers = await fetchFollowers();
+  // displayFollowers(paginate(followers)[0])
+  title.textContent = "Pagination";
+
+  pages = paginate(followers);
+  console.log(pages);
+  setupUI();
+};
+
+btnContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("page-btn")) {
+    console.log(e.target.dataset.index);
+    index = parseInt(e.target.dataset.index);
+    displayButtons(btnContainer, pages, index);
+    setupUI();
+  }
+    if (e.target.classList.contains('next-btn')){
+        index++
+        if (index>pages.length-1) {
+            index = 0
+        }
+        console.log('next');
+        setupUI()
+    }
+    if (e.target.classList.contains('prev-btn')){
+        index--
+        if (index<0) {
+            index = pages.length-1
+        }
+        console.log('prev');
+        setupUI()
+    }
+});
+
+window.addEventListener("load", init);
--- javascript-basic-projects-master/27-pagination/setup/paginate.js
+++ javascript-basic-projects-master/27-pagination/setup/paginate.js
@@ -1,3 +1,12 @@
-const paginate = () => {}
+const paginate = (followers) => {
+    const itemsPerPage = 9
+    const numberOfPages = Math.ceil(followers.length/ itemsPerPage)
+    console.log(numberOfPages);
+    const pages = Array.from({ length: numberOfPages }, (_, index)=> {
+        const start = index * itemsPerPage
+        return followers.slice(start, start+itemsPerPage);
+    })
+    return pages
+}
 
 export default paginate
